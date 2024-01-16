13e6232f75215695bc3cebfca1fb32035aafa3c3
GermanUB31 <145345359+GermanUB31@users.noreply.github.com>
2024-01-16 20:22:57 +0300

    Update практика 9-29 банкомат.py

--- практика 9-29 банкомат.py
+++ практика 9-29 банкомат.py
@@ -1,9 +1,26 @@
 from tkinter import *
+from tkinter import messagebox
 window = Tk()
 window.attributes('-fullscreen', True)
 window.title('интерфейс банкомата НеобманБАНК')
 
 
+#Важные переменные
+adminlogin = 'ADMIN'
+pin_code = '0000'
+balance = '0'
+
+def false_pin():                                                          #СООБЩЕНИЕ "НЕВЕРНЫЙ ПИН"
+    global n
+    if n > 0:
+        messagebox.showinfo('Внимание!','ПИН-код не верный')
+        pin.delete(0,END)
+    else:
+        vvbut.destroy()
+        pin.destroy()
+        vvpin.destroy()
+        nachalo()
+        n = n+3
 
 def nachalo():                                                                           #НАЧАЛЬНОЕ ОКНО
     lbl1 = Label(window, text='НеобманБАНК', font=('Arial Bold', 40))
@@ -19,6 +36,11 @@ def nachalo():                                                                  
     global nachbutt
     nachbutt = Button(window, text='Начать --->', font=('Arial Bold', 40), command= pincode)
     nachbutt.place(relx=0.5, rely=0.5,anchor=CENTER)
+    admbut = Button(window, text='Для администратора', font=('Arial Bold', 10), command= admin_menu)
+    admbut.place(x=745, y=820)
+
+def admin_menu():
+    nachbutt.destroy()
 
 def pincode():                                                                         #ВВОД ПИН-КОДА
     def press_key(event):
@@ -43,10 +65,26 @@ def pincode():                                                                  
     vvbut = Button(window, text='Ввод \n (ENTER)', font=('Arial Bold', 16), command=dialogwindow)
     vvbut.place(x=1070, y=400)
 
-def dialogwindow():
-    vvbut.destroy()
-    pin.destroy()
-    vvpin.destroy()
+n = 3
+
+def dialogwindow():                                                              #ДИАЛОГОВОЕ ОКНО БАЛАНС ВВОД ВЫВОД
+    p = pin.get()
+    global n
+    
+    if p == pin_code:
+        vvbut.destroy()
+        pin.destroy()
+        vvpin.destroy()
+        operbut1 = Button(window, text='Узнать баланс карты', font=('Arial Bold', 40))
+        operbut2 = Button(window, text='Ввод средств', font=('Arial Bold', 40))
+        operbut3 = Button(window, text='Вывод средств', font=('Arial Bold', 40))
+        operbut1.place(x=500, y=270)
+        operbut2.place(relx=0.5, rely=0.5,anchor=CENTER)
+        operbut3.place(x=560, y=490)
+    else:
+        n = n-1
+        false_pin()
+
 
 
 
